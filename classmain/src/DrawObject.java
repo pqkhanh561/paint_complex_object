4d25208120f15a05f1b36d8f0efcf4d4573a08b0
khanhphan <pqkhanh561@gmail.com>
2019-12-16 10:28:17 +0700

    Done with func

--- classmain/src/DrawObject.java
+++ classmain/src/DrawObject.java
@@ -7,6 +7,7 @@ import java.awt.geom.Area;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.lang.String;
+import java.lang.Integer;
 
 
 public class DrawObject{
@@ -15,10 +16,10 @@ public class DrawObject{
 	
 	public DrawObject(MyShape shape){
 		arrShape.add(shape);
-		func = " ";
+		func.add("1");
 	}
 
-	public DrawObject(ArrayList<MyShape> arrShape, String func){ 
+	public DrawObject(ArrayList<MyShape> arrShape, ArrayList<String> func){ 
 		this.arrShape = arrShape;
 		this.func = func;
 	}
@@ -57,8 +58,33 @@ public class DrawObject{
 		return(arrShape);
 	}
 
-	public String getfunc(){
+	public ArrayList<String> getfunc(){
 		return(func);
+	}
+
+	private static boolean isNumeric(String strNum) {
+		if (strNum == null) {
+			return false;
+		}
+		try {
+			double d = Double.parseDouble(strNum);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+ 	
+	private ArrayList<String> mark_number_again(ArrayList<String> list){
+		int new_index = 0;
+		for (int i =0; i<list.size(); i++){
+			if (isNumeric(list.get(i))){
+				new_index+=1;
+				//list.replace(str,Integer.toString(new_index));
+				list.set(i, Integer.toString(new_index));
+				//str = Integer.toString(new_index);
+			}
+		}
+		return(list);
 	}
 	
 	//TODO: 1: union
@@ -66,7 +92,17 @@ public class DrawObject{
 		ArrayList<MyShape> arrShape_tmp = new ArrayList<MyShape>();
 		arrShape_tmp.addAll(arrShape);
 		arrShape_tmp.addAll(shape.getarr());
-		String func_tmp = "(" + func + ")+("+ shape.getfunc() + ")";
+
+		//Set new function 
+		ArrayList<String> func_tmp = new ArrayList<String>();
+		func_tmp.add("(");
+		func_tmp.addAll(func);
+		func_tmp.add(")");
+		func_tmp.add("+");
+		func_tmp.add("(");
+		func_tmp.addAll(shape.getfunc());
+		func_tmp.add(")");
+		func_tmp = mark_number_again(func_tmp);
 		DrawObject tmp = new DrawObject(arrShape_tmp, func_tmp);
 		return(tmp);
 	}	
